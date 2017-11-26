47d8ec75cbda1998456c5edc9cc3114c5247cc67
lyldayu <lyldayu@gmail.com>
2017-11-26 00:19:56 -0800

    project UI

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
   var basicOption = {
     title: {
       display: true,
-      text: 'Temperature & Humidity Real-time Data',
+      text: 'CO2 & VOC Real-time Data',
       fontSize: 36
     },
     scales: {
