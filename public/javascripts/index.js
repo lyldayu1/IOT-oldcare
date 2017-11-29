3975bd5ee38a475156615ef85c8534cb892c50f6
lyldayu <lyldayu@gmail.com>
2017-11-28 18:03:03 -0800

    abc

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 
   //Get the context of the canvas element we want to select
   //console.log("234");
-  var x=document.getElementById("demo");  //查找元素
+  var x=document.getElementById("co2_value");  //查找元素
   x.innerHTML = "123";
   var ctx = document.getElementById("myChart").getContext("2d");
   var optionsNoAnimation = { animation: false }
