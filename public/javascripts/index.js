524171d496e23eddbb222019bc537ce1cc47d1e4
lyldayu <lyldayu@gmail.com>
2017-11-28 17:14:09 -0800

    abc

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 
   //Get the context of the canvas element we want to select
   var x=document.getElementById("demo");  //查找元素
-  x="123";
+  x.setContext("123");
   var ctx = document.getElementById("myChart").getContext("2d");
   var optionsNoAnimation = { animation: false }
   var myLineChart = new Chart(ctx, {
