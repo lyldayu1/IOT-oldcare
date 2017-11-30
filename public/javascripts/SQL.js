2c000f5d7d608a23c66f753ea69dfee2c2ea9de7
lyldayu <lyldayu@gmail.com>
2017-11-30 14:26:53 -0800

    add sql

--- public/javascripts/SQL.js
+++ public/javascripts/SQL.js
@@ -1,13 +1,12 @@
+'use strict';
 var azure = require('azure-storage');
 
-
-sql1.prototype.judgetable = function(){
-var tableService = azure.createTableService('oldcaresql','wU1dcVI5ESAcgL490pj/G+JXwIGNqAZl0ulfGIeBeq/X/rz+ZkCazIytA/D8IrsLkbNj9tVDjqNL/slT4KJO5Q==');
+module.exports = function() {
+    var tableService = azure.createTableService('oldcaresql','wU1dcVI5ESAcgL490pj/G+JXwIGNqAZl0ulfGIeBeq/X/rz+ZkCazIytA/D8IrsLkbNj9tVDjqNL/slT4KJO5Q==');
 tableService.createTableIfNotExists('mytable', function(error, result, response) {
   if (!error) {
     // result contains true if created; false if already exists
     console.log("faileee");
   }
 });
-}
-module.exports = sql1;
+};
