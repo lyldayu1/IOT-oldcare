b458c1365cac29ab15a010c2d35100034e81018c
lyldayu <lyldayu@gmail.com>
2017-11-30 15:31:09 -0800

    add sql

--- public/javascripts/SQL.js
+++ public/javascripts/SQL.js
@@ -1,5 +1,5 @@
 'use strict';
-//var azure = require('azure-storage');
+var azure = require('azure-storage');
 
 module.exports = function() {
     var tableService = azure.createTableService('oldcaresql','wU1dcVI5ESAcgL490pj/G+JXwIGNqAZl0ulfGIeBeq/X/rz+ZkCazIytA/D8IrsLkbNj9tVDjqNL/slT4KJO5Q==');
