11a5932f59ef61b6047f914e7c2059bcdc43df62
lyldayu <lyldayu@gmail.com>
2017-11-30 17:20:04 -0800

    sql

--- public/javascripts/SQL.js
+++ public/javascripts/SQL.js
@@ -4,10 +4,19 @@ var azure = require('azure-storage');
 module.exports = function() {
     var tableService = azure.createTableService('oldcaresql','wU1dcVI5ESAcgL490pj/G+JXwIGNqAZl0ulfGIeBeq/X/rz+ZkCazIytA/D8IrsLkbNj9tVDjqNL/slT4KJO5Q==');
 	console.log("faileee");
-	tableService.createTableIfNotExists('mytable', function(error, result, response) {
-  if (!error) {
-    // result contains true if created; false if already exists
-    console.log("faileee");
-  }
-});
-};
+	var entGen = azure.TableUtilities.entityGenerator;
+	var entity = {
+  	PartitionKey: entGen.String('part2'),
+  	RowKey: entGen.String('row1'),
+  	boolValueTrue: entGen.Boolean(true),
+  	boolValueFalse: entGen.Boolean(false),
+  	intValue: entGen.Int32(42),
+  	dateValue: entGen.DateTime(new Date(Date.UTC(2011, 10, 25))),
+  	complexDateValue: entGen.DateTime(new Date(Date.UTC(2013, 02, 16, 01, 46, 20)))
+	};
+	tableService.insertEntity('mytable', entity, function(error, result, response) {
+  	if (!error) {
+    // result contains the ETag for the new entity
+  	}
+	});
+}
