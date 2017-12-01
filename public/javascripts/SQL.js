b0e960d05811a68134b2343614210a5698fe8eb2
lyldayu <lyldayu@gmail.com>
2017-11-30 16:08:46 -0800

    sql

--- public/javascripts/SQL.js
+++ public/javascripts/SQL.js
@@ -3,7 +3,8 @@ var azure = require('azure-storage');
 
 module.exports = function() {
     var tableService = azure.createTableService('oldcaresql','wU1dcVI5ESAcgL490pj/G+JXwIGNqAZl0ulfGIeBeq/X/rz+ZkCazIytA/D8IrsLkbNj9tVDjqNL/slT4KJO5Q==');
-tableService.createTableIfNotExists('mytable', function(error, result, response) {
+	console.log("faileee");
+	tableService.createTableIfNotExists('mytable', function(error, result, response) {
   if (!error) {
     // result contains true if created; false if already exists
     console.log("faileee");
